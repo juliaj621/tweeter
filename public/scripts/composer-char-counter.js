68cac3eba74b8574787774e2f4d0bfa5582a2362
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-08 14:14:32 -0700

    implemented character counter feature

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('#tweetTextArea').keyup(function() {
+    const text = $(this).val();
+    const buttonValue = 140;
+    $(this).siblings(".form-button").children(".counter").text(buttonValue - text.length);
+    if (text.length > 140) {
+      $(this).siblings(".form-button").children(".counter").css("color", "red");
+    } else {
+      $(this).siblings(".form-button").children(".counter").css("color", "#545149");
+    }
+  })
+});
