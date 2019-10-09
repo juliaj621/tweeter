b622dee6171535d34472685f6e9e6cd4266fde9a
Julia Jachimowicz <juliaj621@gmail.com>
2019-10-09 14:20:41 -0700

    added ability to submit tweets using ajax

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,32 +1,26 @@
-// Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+$("#tweet-submit").submit(function( event ) {
+  event.preventDefault();
+  console.log($( this ).serialize());
+  $.ajax({ method: 'POST', url: '/tweets', data: $( this ).serialize()})
+  .then(function (data) {
+    console.log('Success: ', data);
+    loadTweets(data)
+  });
+});
+
+const loadTweets = function () {
+  $.ajax({method: 'GET', url: '/tweets', dataType: 'JSON'})
+  .then(res => {
+    console.log('Success: ', renderTweets(res))
+  });
+};
+
+loadTweets()
 
 const renderTweets = function(tweets) {
-    for (let x = 0; x < tweets.length; x++) {
-    let newTweet = createTweetElement(tweets[x])
+  $('#tweets-container').empty();
+  for (let i = 0; i < tweets.length; i++) {
+    let newTweet = createTweetElement(tweets[i])
     $('#tweets-container').append(newTweet);
   }
 };
@@ -51,5 +45,3 @@ const createTweetElement = function(tweetObject) {
     </article>`)
   return $tweet;
 }
-
-renderTweets(data)
